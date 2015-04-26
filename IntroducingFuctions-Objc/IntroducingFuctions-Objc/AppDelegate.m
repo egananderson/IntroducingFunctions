33dc1f7f6ca5c6644d8092124bb876c780e8a249
Egan Anderson <p.egan.anderson@gmail.com>
2015-04-25 19:19:58 -0600

    new methods

--- IntroducingFuctions-Objc/IntroducingFuctions-Objc/AppDelegate.m
+++ IntroducingFuctions-Objc/IntroducingFuctions-Objc/AppDelegate.m
@@ -17,11 +17,31 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 
-    // Insert code here...
-    
-    
+    NSString *myName = @"Egan";
+    countDownTillIntroduction(4);
+    printIntroductions(myName);
+
     return YES;
 }
+
+void printIntroductions(NSString *name){
+    NSString *introduction = [NSString stringWithFormat: @"My name is %@", name];
+    NSString *japaneseIntroduction = [name stringByAppendingString: @" to moushimasu"];
+    
+    NSLog(@"%@", introduction);
+    NSLog(@"%@", japaneseIntroduction);
+}
+
+void countDownTillIntroduction(int numberOfDays){
+    if(numberOfDays == 0){
+        NSLog(@"The time has come");
+    }else{
+        NSLog(@"%d days left until introductions", numberOfDays);
+        int oneLessDay = numberOfDays - 1;
+        countDownTillIntroduction(oneLessDay);
+        }
+}
+
 
 // Define new functions here...
 
